a7d2c7fd04fb89da5be92f68657fad569e49a470
suhyun <1eesuhyun521@gmail.com>
2024-11-15 17:30:41 +0900

    vue, django 폴더 생성 및 라이브러리 추가

---
+++ vue-project/want-card/src/main.js
@@ -0,0 +1,18 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
+const app = createApp(App)
+const pinia = createPinia()
+
+pinia.use(piniaPluginPersistedstate)
+// app.use(createPinia())
+app.use(pinia)
+app.use(router)
+
+app.mount('#app')
